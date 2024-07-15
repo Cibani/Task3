965869add48946174d50c90fb0b105d642f9e699
Cibani <151187399+Cibani@users.noreply.github.com>
2024-07-15 22:51:16 +0530

    Add files via upload

---
+++ task3/server.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+// initialising variables
+$username ="";
+$email ="";
+$errors ="";
+
+//connect to the database
+$db = mysqli_connect('localhost', 'root', 'Cibani3003#','bms');
+
+
+//REGISTER USER
+if (isset($_POST['reg_user'])) {
+    //receive all input values from the form
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    //form validation: ensure that form is correctly filled...
+    //by adding (array_push()) corresponding error unto $errors array
+    if (empty($username)) {array_push($errors, "Username is required"); }
+    if (empty($email)) {array_push($errors, "Email is required"); }
+    if (empty($password_1)) {array_push($errors, "Password is required"); }
+    if ($password_1 != $password_2) {array_push($errors, "Password mismatch"); }
+
+
+}
